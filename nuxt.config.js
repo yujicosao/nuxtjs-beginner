5d13f12dce33a574cdf3e5253b8c3827c3209dc8
yujicosao <yujikosao@me.com>
2019-11-12 15:57:54 +0900

    プラグインを利用する(Elementライブラリー)

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/element.js'
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ pages/element.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="padding:30px;">
+        <el-button @click="visible = true">タイアログを開く</el-button>
+        <el-dialog v-bind:visible.sync="visible" title="確認">
+            <p>処理されました</p>
+        </el-dialog>
+    </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return{
+            visible:false
+        }
+    }
+}
+</script>
